58a7e248b14a13283d01cdab59522748acbdda20
Val-Addy <valentinoaddy@gmail.com>
2023-11-16 10:02:10 -0800

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,18 +7,20 @@
  *
  * Return: the number of nodes printed
  */
+
 size_t print_list(const list_t *h)
+
 {
 	size_t s = 0;
 
 	while (h)
-{
+	{
 	if (!h->str)
 	printf("[0] (nil)\n");
 	else
 	printf("[%u] %s\n", h->len, h->str);
 	h = h->next;
-	i s++;
+	s++;
 }
 
 	return (s);
